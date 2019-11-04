c10f87ee653245d6669165a55a2ef46b517c1392
wsj <scglwsj@163.com>
2019-11-04 14:24:33 +0800

    [refactor] rename package

--- src/main/kotlin/com/askjamie/latvia/hello/HelloController.kt
+++ src/main/kotlin/com/askjamie/latvia/hello/HelloController.kt
@@ -1,4 +1,4 @@
-package com.askjamie.actionservice.hello
+package com.askjamie.latvia.hello
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
